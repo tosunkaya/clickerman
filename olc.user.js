7d598d67c64e42aa8d881e02c370acaa529dda45
Kafva <jonas@kafva.one>
2022-09-11 11:56:28 +0200

    -

--- olc.user.js
+++ olc.user.js
@@ -14,37 +14,38 @@
 // 	file:///Users/jonas/Repos/clickerman/olc.user.js
 
 // https://www.overleaf.com/learn/how-to/How_can_I_define_custom_Vim_macros_in_a_vimrc_file_on_Overleaf%3F#Example_script_with_TamperMonkey_to_add_keybindings
+'use strict';
+
 (() => {
-	'use strict';
 	// poll until editor is loaded
 	const retry = setInterval(() => {
-	if (window._debug_editors === undefined) return
-	clearInterval(retry)
+		if (window._debug_editors === undefined) return
+		clearInterval(retry)
 
-	// get current editor instance
-	const editor = window._debug_editors[window._debug_editors.length -1]
+		// get current editor instance
+		const editor = window._debug_editors[window._debug_editors.length -1]
 
-	// vim keyboard plugin
-	const vimKeyboard = window.ace.require("ace/keyboard/vim")
+		// vim keyboard plugin
+		const vimKeyboard = window.ace.require("ace/keyboard/vim")
 
-	vimKeyboard.Vim.map("Y", "y$", "normal")
+		vimKeyboard.Vim.map("Y", "y$", "normal")
 	
-	// Graphical line movement
-	vimKeyboard.Vim.map("k", "gk", "normal")
-	vimKeyboard.Vim.map("j", "gj", "normal")
-	vimKeyboard.Vim.map("<Up>", "gk", "normal")
-	vimKeyboard.Vim.map("<down>", "gj", "normal")
-	vimKeyboard.Vim.map("<up>", "<Esc>gki", "insert")
-	vimKeyboard.Vim.map("<down>", "<Esc>gji", "insert")
+		// Graphical line movement
+		vimKeyboard.Vim.map("k", "gk", "normal");
+		vimKeyboard.Vim.map("j", "gj", "normal");
+		vimKeyboard.Vim.map("<Up>", "gk", "normal");
+		vimKeyboard.Vim.map("<down>", "gj", "normal");
+		vimKeyboard.Vim.map("<up>", "<Esc>gki", "insert");
+		vimKeyboard.Vim.map("<down>", "<Esc>gji", "insert");
 
-	vimKeyboard.Vim.map("<C-a>", "0", "normal")
-	vimKeyboard.Vim.map("<C-e>", "$", "normal")
-	vimKeyboard.Vim.map("<C-a>", "<Esc>0i", "insert")
-	vimKeyboard.Vim.map("<C-e>", "<Esc>$i", "insert")
+		vimKeyboard.Vim.map("<C-a>", "0", "normal");
+		vimKeyboard.Vim.map("<C-e>", "$", "normal");
+		vimKeyboard.Vim.map("<C-a>", "<Esc>0i", "insert");
+		vimKeyboard.Vim.map("<C-e>", "<Esc>$i", "insert");
 
-	// set the modified keyboard handler for editor
-	editor.setKeyboardHandler(vimKeyboard.handler)
+		// set the modified keyboard handler for editor
+		editor.setKeyboardHandler(vimKeyboard.handler);
 
-	console.log("Custom key bindings applied")
-}, 1000)
+		console.log("Custom key bindings applied");
+	}, 1000)
 })();
