c4fb9cfae8e58617ca3fff3e4a23b4b7e336f870
Kafva <jonas@kafva.one>
2022-09-11 11:56:28 +0200

    -

--- click.user.js
+++ click.user.js
@@ -29,8 +29,6 @@ const GOOGLE_CONSENT = "[aria-label='Show me the privacy reminder later'],[aria-
 const TWITCH_BONUS = ".claimable-bonus__icon.tw-flex"
 const YT_STILL_WATCHING = ".style-scope.yt-button-renderer.style-blue-text.size-default[aria-label='Yes']"
 
-const IS_LIVESTREAM = document.querySelector("yt-live-chat-renderer") != null
-
 /// Note that some pop-ups may are iframes from different domainis (e.g. consent.youtube.com)
 /// to click these requires { "all_frames": true } and the
 /// domain to appear in the "matches" inside manifest.json
@@ -77,14 +75,25 @@ const get_agree_button_selector = () => {
 	return selector == "" ? "" : "," + selector
 }
 
-if ( window.location.host.match(".*.(youtube|google).com") ){
-	if (!IS_LIVESTREAM){
-		auto_click([YT_STILL_WATCHING, GOOGLE_CONSENT  + get_agree_button_selector() ]);
-	} 
-}
-else if ( window.location.host.match(".*.twitch.com") ){
-	auto_click([TWITCH_BONUS]);
+
+window.onload = () => {
+	const IS_LIVESTREAM = document.querySelector(".view-count")?.innerText.match(/watching now/) != null
+	const IS_IFRAME     = window.self != window.top
+	
+	if ( window.location.host.match(".*.(youtube|google).com") ){
+		if (!IS_LIVESTREAM && !IS_IFRAME){
+			auto_click([YT_STILL_WATCHING, GOOGLE_CONSENT  + get_agree_button_selector() ]);
+		} 
+	}
+	else if ( window.location.host.match(".*.twitch.com") ){
+		auto_click([TWITCH_BONUS]);
+	}
+
+	// Initialised
+	debug("clickerman is running..." + 
+		(IS_LIVESTREAM ? " (livestream)" : "") +
+		(IS_IFRAME ? " (iframe)" : "")
+	);
+
 }
 
-// Initialised
-debug(`clickerman is${!IS_LIVESTREAM ? "" : " not"} running...`);
